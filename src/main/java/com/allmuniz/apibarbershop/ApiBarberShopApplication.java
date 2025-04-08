d728fae16ddb6612634009d5309d04bf9dfb91b2
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-04-08 09:11:48 -0300

    first commit

---
+++ src/main/java/com/allmuniz/apibarbershop/ApiBarberShopApplication.java
@@ -0,0 +1,13 @@
+package com.allmuniz.apibarbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBarberShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBarberShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allmuniz/apibarbershop/ApiBarberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allmuniz.apibarbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBarberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
